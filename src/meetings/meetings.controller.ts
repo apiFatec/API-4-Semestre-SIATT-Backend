ceaa7d9bd0cf08c3270a2d95ba92af4817e47ba6
Caboia <caio.rodriguesyokoyama@gmail.com>
2024-04-11 11:53:00 -0300

    :sparkles: feat: API Zoom

--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query, Redirect } from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 import { ZoomService } from 'src/zoom/zoom.service';
 
@@ -6,13 +6,31 @@ import { ZoomService } from 'src/zoom/zoom.service';
 export class MeetingsController {
   constructor(private readonly zoomService: ZoomService) {}
 
-  @Post('create-meeting')
-  async createMeeting(@Body() body: { topic: string }) {
-    try {
-      const meeting = await this.zoomService.createMeeting(body.topic);
-      return { success: true, meeting };
-    } catch (error) {
-      return { success: false, error: error.message };
+    @Get('authorize')
+    @Redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=4wYTcnqGRYSWytTTBOoTYw&redirect_uri=http://localhost:3000/meetings/callback')
+    authorize() {
     }
+
+    @Get('callback')
+async handleAuthorizationCallback(@Query('code') code: string, @Query('state') state: string) {
+  try {
+    const params = new URLSearchParams(state);
+    const topic = params.get('topic');
+    const startDate = params.get('start_date');
+    const durationString = params.get('duration');
+
+    const duration = parseInt(durationString);
+
+    const accessToken = await this.zoomService.getAccessToken(code);
+    console.log('Access Token:', accessToken);
+
+    const meeting = await this.zoomService.createMeeting(topic, startDate, duration, accessToken);
+
+    return { success: true, meeting };
+  } catch (error) {
+    console.error('Erro ao obter o token de acesso ou criar a reunião:', error.response?.data || error.message);
+    return { success: false, error: 'Erro ao criar a reunião' };
   }
 }
+  }
+   
--- src/zoom/zoom.service.ts
+++ src/zoom/zoom.service.ts
@@ -6,14 +6,15 @@ require('dotenv').config();
 export class ZoomService {
   private readonly zoomApiBaseUrl = 'https://api.zoom.us/v2';
 
-  async createMeeting(topic: string): Promise<any> {
+  async createMeeting(topic: string, startDate: string, duration: number, accessToken: string): Promise<any> {
     try {
-      const accessToken = await this.getToken();
       console.log('Access Token:', accessToken);
       const response = await axios.post(
         `${this.zoomApiBaseUrl}/users/me/meetings`,
         {
           topic,
+          start_time: startDate,
+          duration,
           type: 2,
         },
         {
@@ -22,7 +23,7 @@ export class ZoomService {
           },
         }
       );
-
+  
       return response.data;
     } catch (error) {
       console.error('Erro ao criar a reunião:', error.response?.data || error.message);
@@ -30,20 +31,27 @@ export class ZoomService {
     }
   }
 
-  private async getToken(): Promise<string> {
+  async getAccessToken(code: string): Promise<string> {
     try {
-      const response = await axios.post('https://zoom.us/oauth/token', 
-        `grant_type=client_credentials&client_id=${process.env.ZOOM_CLIENT_ID}&client_secret=${process.env.ZOOM_CLIENT_SECRET}`,
+      const requestBody = new URLSearchParams();
+      requestBody.append('grant_type', 'authorization_code');
+      console.log(code)
+      requestBody.append('code', code);
+      requestBody.append('redirect_uri', 'http://localhost:3000/meetings/callback');
+
+      const response = await axios.post(
+        'https://zoom.us/oauth/token',
+        requestBody.toString(),
         {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic NHdZVGNucUdSWVNXeXRUVEJPb1RZdzpkQ3BNSXlWQXc2ZzZ4YThUSmtNcUYyREY2bFNvN1BXbA==`,
           },
-        });
+        }
+      );
 
-      const accessToken = response.data.access_token;
-      console.log('Access Token:', accessToken);
-
-      return accessToken;
+      console.log(response.data.access_token);
+      return response.data.access_token;
     } catch (error) {
       console.error('Erro ao obter o token de acesso:', error.response?.data || error.message);
       throw error;
